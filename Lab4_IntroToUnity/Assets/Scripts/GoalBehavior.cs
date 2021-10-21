9a3ba28a539bd478b176ac299c6669227fc4ece4
perenewang <perenewang@users.noreply.github.com>
2021-10-21 16:34:31 -0500

    adding folder

---
+++ Lab4_IntroToUnity/Assets/Scripts/GoalBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBehavior : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+       //Put collision code here
+       if (collision.gameObject.name == "Marble")
+       {
+         Destroy(this.transform.gameObject);
+       }
+    }
+}
